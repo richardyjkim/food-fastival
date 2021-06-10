8046ed53ad91f4a2f8b47c1803d45604d46cbcf5
Richard <richardyjkim@gmail.com>
2021-06-10 14:14:35 -0700

    optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const path = require("path");
-const webpack = require("webpack");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
-module.exports = {
+const config = {
   entry: {
     app: './assets/js/script.js',
     events: './assets/js/events.js',
@@ -11,23 +12,23 @@ module.exports = {
     tickets: './assets/js/tickets.js'
   },
   output: {
-    path: __dirname + '/dist',
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    path: `${__dirname}/dist`
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name(file) {
-                return '[path][name].[ext]'
+                return '[path][name].[ext]';
               },
-              publicPath: function (url) {
-                return url.replace("../", "/assets/")
+              publicPath(url) {
+                return url.replace('../', '/assets/');
               }
             }
           },
@@ -40,27 +41,31 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static', //the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static'
     }),
     new WebpackPwaManifest({
-      name: "Food Event",
-      short_name: "Foodies",
-      description: " An app that allows tou to view upcoming food events.",
-      start_url: "../index.html",
-      background_color: "#ffffff",
+      name: 'Food Event',
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming food events.',
+      start_url: '../index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
       fingerprints: false,
       inject: false,
-      icons: [{
-        src: path.resolve("assets/img/icons/icon-512x512.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join("assets", "icons")
-      }]
+      icons: [
+        {
+          src: path.resolve('assets/img/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }
+      ]
     })
   ],
   mode: 'development'
 };
 
+module.exports = config;
